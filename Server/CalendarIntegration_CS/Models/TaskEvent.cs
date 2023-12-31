4d493246f5fee80793930b4130307716ab496c4a
Lob26 <p.lobato@uniandes.edu.co>
2023-12-31 15:05:50 -0500

    CS testing

---
+++ Server/CalendarIntegration_CS/Models/TaskEvent.cs
@@ -0,0 +1,8 @@
+namespace CalendarIntegration_CS.Models;
+
+public class TaskEvent
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Deadline { get; set; }
+}
